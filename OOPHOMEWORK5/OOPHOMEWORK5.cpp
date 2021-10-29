d727aa3e7735e36e86d77f7edaabea2789956211
IMBABALANCE <IMBABALANCE@DESKTOP-4JCDH27>
2021-10-30 00:51:50 +0900

    Добавьте файлы проекта.

---
+++ OOPHOMEWORK5/OOPHOMEWORK5.cpp
@@ -0,0 +1,141 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void obzac()
+{
+    cout << "\n\n//====================================HomeWork====================================//\n\n";
+}
+void obzac1()
+{
+    cout << "\n\n//======================================END======================================//\n\n";
+}
+void obzac2()
+{
+    cout << "\n\n//-------------------------------------------------------------------------------//\n\n";
+}
+
+/*HOMEWORK1
+Реализовать шаблон класса Pair1, 
+который позволяет пользователю передавать данные одного типа парами.
+int main()
+{
+    Pair1<int> p1(6, 9);
+    cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
+
+    const Pair1<double> p2(3.4, 7.8);
+    cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+
+    return 0;
+
+	… должен производить результат:
+    Pair: 6 9
+    Pair: 3.4 7.8
+}
+*/
+template <class T>
+class Pair1
+{
+private:
+	T m_a;
+	T m_b;
+
+public:
+	Pair1(const T& a, const T& b): m_a(a), m_b(b){}
+
+	T& first() { return m_a; }
+	const T& first() const { return m_a; }
+	T& second() { return m_b; }
+	const T& second() const { return m_b; }
+};
+
+/*HOMEWORK2
+Реализовать класс Pair, который позволяет использовать разные типы данных в передаваемых парах.
+Следующий код:
+int main()
+{
+	Pair<int, double> p1(6, 7.8);
+	cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
+
+	const Pair<double, int> p2(3.4, 5);
+	cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+
+	return 0;
+}
+… должен производить следующий результат:
+Pair: 6 7.8
+Pair: 3.4 5
+Подсказка: чтобы определить шаблон с использованием двух разных типов, просто разделите параметры типа шаблона запятой.
+*/
+template <class T, class S>
+class Pair
+{
+private:
+	T m_a;
+	S m_b;
+
+public:
+	Pair(const T& a, const S& b): m_a(a), m_b(b){}
+
+	T& first() { return m_a; }
+	const T& first() const { return m_a; }
+	S& second() { return m_b; }
+	const S& second() const { return m_b; }
+};
+
+/*Написать шаблон класса StringValuePair, в котором первое значение всегда типа string, а второе — любого типа. 
+Этот шаблон класса должен наследовать частично специализированный класс Pair, в котором первый параметр — string, а второй — любого типа данных.
+Следующий код:
+int main()
+{
+	StringValuePair<int> svp("Amazing", 7);
+	std::cout << "Pair: " << svp.first() << ' ' << svp.second() << '\n';
+	return 0;
+}
+… должен производить следующий результат:
+Pair: Amazing 7
+Подсказка: при вызове конструктора класса Pair из конструктора класса StringValuePair не забудьте указать, что параметры относятся к классу Pair.
+
+
+*/
+template <class S>
+class StringValuePair : public Pair<string, S>
+{
+public:
+	StringValuePair(const string& key, const S& value): Pair<string, S>(key, value){}
+};
+
+
+
+int main()
+{
+	{
+		//HOMEWORK1
+		obzac();
+		Pair1<int> p1(6, 9);
+		cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
+		const Pair1<double> p2(3.4, 7.8);
+		cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+		obzac2();
+	}
+
+	{
+		//HOMEWORK2
+		obzac();
+		Pair<int, double> p1(6, 7.8);
+		cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
+
+		const Pair<double, int> p2(3.4, 5);
+		cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+		obzac2();
+	}
+
+	{
+		//HOMEWORK3
+		obzac();
+		StringValuePair<int> svp("Amazing", 7);
+		cout << "Pair: " << svp.first() << ' ' << svp.second() << '\n';
+		obzac1();
+	}
+	return 0;
+};
---
+++ OOPHOMEWORK5/blackjack.h
@@ -0,0 +1,134 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/*HOMEWORK3
+–еализовать класс Hand, который представл€ет собой коллекцию карт.
+¬ классе будет одно поле: вектор указателей карт (удобно использовать вектор, т.к. это по сути динамический массив,
+а тип его элементов должен быть - указатель на объекты класса Card).
+“акже в классе Hand должно быть 3 метода:
+метод Add, который добавл€ет в коллекцию карт новую карту, соответственно он принимает в качестве параметра указатель на новую карту
+метод Clear, который очищает руку от карт
+метод GetValue, который возвращает сумму очков карт руки (здесь предусмотреть возможность того, что туз может быть равен 11).
+*/
+/*HOMRWORK4
+—огласно иерархии классов, котора€ представлена в методичке к уроку 3, от класса Hand наследует класс GenericPlayer, 
+который обобщенно представл€ет игрока, ведь у нас будет два типа игроков - человек и компьютер. —оздать класс GenericPlayer, 
+в который добавить поле name - им€ игрока. “акже добавить 3 метода:
+IsHitting() - чисто виртуальна€ функци€, возвращает информацию, нужна ли игроку еще одна карта.
+IsBoosted() - возвращает bool значение, есть ли у игрока перебор
+Bust() - выводит на экран им€ игрока и объ€вл€ет, что у него перебор.
+*/
+class Card
+{
+public:
+	enum rank { ACE = 1, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING };
+	enum suit { CLUBS, DIAMONDS, HEARTS, SPADES };
+
+	Card(suit s, bool ifu = true, rank r = ACE) : m_Rank(r), m_Suit(s), m_IsFaceUp(ifu) { }
+
+	int GetValue() const
+	{
+		int value = 0;
+		if (m_IsFaceUp)
+		{
+			value = m_Rank;
+			if (value > 10)
+				value = 10;
+		}
+		return value;
+	}
+
+	void Flip()
+	{
+		m_IsFaceUp = !m_IsFaceUp;
+	}
+
+	friend ostream& operator<<(ostream& s, const Card& aCard);
+
+private:
+	rank m_Rank;
+	suit m_Suit;
+	bool m_IsFaceUp;
+};
+
+class Hand
+{
+public:
+	Hand()
+	{
+		m_Cards.reserve(7);
+	}
+
+	virtual ~Hand()
+	{
+		Clear();
+	}
+
+	void Add(Card* pCard)
+	{
+		m_Cards.push_back(pCard);
+	}
+
+	void Clear()
+	{
+		vector<Card*>::iterator iter = m_Cards.begin();
+		for (iter = m_Cards.begin(); iter != m_Cards.end(); ++iter)
+		{
+			delete* iter;
+			*iter = nullptr;
+		}
+
+		m_Cards.clear();
+	}
+
+	int GetTotal() const
+	{
+		if (m_Cards.empty() || m_Cards[0]->GetValue() == 0)
+			return 0;
+
+		int total = 0;
+		vector<Card*>::const_iterator iter;
+
+		for (iter = m_Cards.begin(); iter != m_Cards.end(); ++iter)
+			total += (*iter)->GetValue();
+
+		bool containsAce = false;
+		for (iter = m_Cards.begin(); iter != m_Cards.end(); ++iter)
+			if ((*iter)->GetValue() == Card::ACE)
+				containsAce = true;
+
+		if (containsAce && total <= 11) total += 10;
+
+		return total;
+	}
+
+protected:
+	vector<Card*> m_Cards;
+};
+
+class GenericPlayer : public Hand
+{
+private:
+	friend ostream& operator<<(ostream& os, const GenericPlayer& aGenericPlayer);
+protected:
+	string m_Name;
+public:
+	GenericPlayer(const string& name = "") : m_Name(name) { }
+
+	virtual ~GenericPlayer();
+
+	virtual bool IsHitting() const = 0;
+
+	bool IsBusted() const
+	{
+		return (GetTotal() > 21);
+	}
+
+	void Bust() const
+	{
+		cout << m_Name << " перебор.\n";
+	}
+};
